9825838dfb57caa337a88e039eb7be087e25e848
Jordan Ehara <jordan.ehara@gmail.com>
2025-06-14 13:51:08 -0700

    Add ice spear attack

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,17 +2,23 @@ extends CharacterBody2D
 
 @export var movement_speed = 20.0
 @export var hp = 10
+@export var knockback_recovery = 3.5
+var knockback = Vector2.ZERO
 
 @onready var player = get_tree().get_first_node_in_group("player")
 @onready var sprite = $Sprite2D
 @onready var anim = $AnimationPlayer
 
+signal remove_from_array(object)
+
 func _ready():
 	anim.play("walk")
 
 func _physics_process(_delta: float) -> void:
+	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
 	var direction = global_position.direction_to(player.global_position)
 	velocity = direction * movement_speed
+	velocity += knockback
 	move_and_slide()
 	
 	if direction.x > 0.1:
@@ -21,7 +27,9 @@ func _physics_process(_delta: float) -> void:
 		sprite.flip_h = false
 
 
-func _on_hurt_box_hurt(damage: Variant) -> void:
+func _on_hurt_box_hurt(damage, angle, knockback_amount) -> void:
 	hp -= damage
+	knockback = angle * knockback_amount
 	if hp <= 0:
+		emit_signal("remove_from_array", self)
 		queue_free() # delete enemy
--- Player/player.gd
+++ Player/player.gd
@@ -3,8 +3,27 @@ extends CharacterBody2D
 var movement_speed = 50.0
 var hp = 80
 
+# Attacks
+var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
+
+# Attack Nodes
+@onready var iceSpearTimer = get_node("%IceSpearTimer")
+@onready var iceSpearAttackTimer = get_node("%IceSpearAttackTimer")
+
+# Ice Spear
+var icespear_ammo = 0
+var icespear_baseammo = 1
+var icespear_attackspeed = 1.5
+var icespear_level = 1
+
+# Enemy Related
+var enemy_close = []
+
 @onready var sprite = $Sprite2D
 @onready var walkTimer = get_node("%walkTimer")
+
+func _ready():
+	attack()
 
 # delta = 1s/frame_rate
 # if moving 600 pixels, movement_speed = 6
@@ -16,8 +35,7 @@ var hp = 80
 # 20 fps = 600/(20*360/20) = 1.67s
 func _physics_process(_delta: float) -> void: # 1/60s movement runs
 	movement()
-	
-	
+
 func movement():
 	 # Takes care of pressing down multiple movement directions. Will be +1 or -1 otherwise
 	var x_mov = Input.get_action_strength("right") - Input.get_action_strength("left")
@@ -43,7 +61,48 @@ func movement():
 	velocity = mov.normalized() * movement_speed 
 	move_and_slide() # specific to 2d body. Automatically uses delta 
 
+func attack():
+	if icespear_level > 0:
+		iceSpearTimer.wait_time = icespear_attackspeed
+		if iceSpearTimer.is_stopped():
+			iceSpearTimer.start()
 
-func _on_hurt_box_hurt(damage: Variant) -> void:
+func _on_hurt_box_hurt(damage, _angle, _knockback) -> void:
 	hp -= damage
 	print(hp)
+
+
+func _on_ice_spear_timer_timeout() -> void:
+	icespear_ammo += icespear_baseammo
+	iceSpearAttackTimer.start()
+
+
+func _on_ice_spear_attack_timer_timeout() -> void:
+	if icespear_ammo > 0:
+		var icespear_attack = iceSpear.instantiate()
+		icespear_attack.position = position
+		icespear_attack.target = get_random_target()
+		icespear_attack.level = icespear_level
+		add_child(icespear_attack)
+		icespear_ammo -= 1
+		if icespear_ammo > 0:
+			iceSpearAttackTimer.start()
+		else:
+			iceSpearAttackTimer.stop()
+
+
+func get_random_target():
+	if enemy_close.size() > 0:
+		return enemy_close.pick_random().global_position
+	else:
+		return Vector2.UP
+
+
+func _on_enemy_detection_area_body_entered(body: Node2D) -> void:
+	if not enemy_close.has(body):
+		enemy_close.append(body)
+
+
+func _on_enemy_detection_area_body_exited(body: Node2D) -> void:
+	if enemy_close.has(body):
+		enemy_close.erase(body)
