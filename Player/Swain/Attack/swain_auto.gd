3071119e7f04c43929ceaf309f54103dd138db27
Jordan Ehara <jordan.ehara@gmail.com>
2025-06-27 11:32:50 -0700

    Add swain auto attack. Add animation, sound, collision polygon

--- Player/Swain/Attack/swain_auto.gd
+++ Player/Swain/Attack/swain_auto.gd
@@ -2,18 +2,17 @@ extends Area2D
 
 var damage = 5
 var attack_size = 1.0
-var HurtBoxType = 0 # Cooldown
+var HurtBoxType = 1 # HitOnce
 
 @onready var swain = get_tree().get_first_node_in_group("player")
-@onready var snd = $snd_ultimate
+@onready var snd = $snd_auto
+@onready var animation = $AnimationPlayer
 
 func _ready():
 	snd.play()
+	animation.play("swain_auto")
 	attack_size = 1.0 * (1 + swain.spell_size)
 	scale = Vector2(1, 1) * attack_size
 
 func _physics_process(_delta: float) -> void:
 	position = swain.global_position.normalized()
-
-func _on_duration_timer_timeout() -> void:
-	queue_free()
