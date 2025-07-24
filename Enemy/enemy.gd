585da4a095fa52a9defefe95768e95f9b33304a0
Jordan Ehara <jordan.ehara@gmail.com>
2025-07-24 12:30:21 -0700

    Add enemy pathfinding.

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -13,6 +13,7 @@ var slow_percentage = 1.0
 @onready var sprite = $Sprite2D
 @onready var anim = $AnimationPlayer
 @onready var hitBox = $HitBox
+@onready var navAgent = $EnemyNavAgent
 
 var exp_gem = preload("res://Objects/experience_gem.tscn")
 
@@ -23,13 +24,18 @@ func _ready():
 	z_index = 1
 	hitBox.damage = enemy_damage
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	knockback = knockback.move_toward(Vector2.ZERO, knockback_recovery)
-	var direction = global_position.direction_to(player.global_position)
-	velocity = direction * movement_speed
+	
+	navAgent.target_position = player.global_position
+	if navAgent.is_navigation_finished():
+		return
+	var next_path_position = navAgent.get_next_path_position()
+	var direction = global_position.direction_to(next_path_position)
+	velocity = global_position.direction_to(next_path_position) * movement_speed
+	velocity = direction * movement_speed * slow_percentage
 	velocity += knockback
-	velocity *= slow_percentage
-	move_and_slide()
+	position += velocity * delta
 	
 	if direction.x > 0.1:
 		sprite.flip_h = true
